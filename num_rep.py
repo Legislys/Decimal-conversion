417ea4bd39f8d1e82b6edcdb5492476bbed687c5
Sasza Węsierski <alewesij@onet.pl>
2023-10-26 16:45:35 +0200

    Remove redudant text

--- num_rep.py
+++ num_rep.py
@@ -16,13 +16,3 @@ test_values = [1157,5226,12166,12649]
 for x in test_values:
     rep = convert_decimal_to_base(x,23)
     print(f'({rep})_23 = ({x})_10')
-
-
-"""
-Szanowny Panie doktorze, 
-
-jestem studentem I roku Matematyki stosowanej grupy 3. Uzgodniliśmy, że nie muszę uczestniczyć w laboratoriach. Nie jestem w stanie zapisać się już dziś do projektu, wyświetla się komunikat, że nie należę do grupy lab. czwartek, co uniemożliwia mi przesłanie zadanie przez platformę Wikamp. Dlatego załączam link do githuba z rozwiązaniami: https://github.com/Legislys/Decimal-conversion
-
- Z poważaniem,
-Aleksander Węsierski
-"""
