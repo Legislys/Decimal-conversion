386a4c3d904029d6afa09338917fc8d029c3e210
Sasza Węsierski <alewesij@onet.pl>
2023-10-26 16:44:39 +0200

    Add index error prevention

--- num_rep.py
+++ num_rep.py
@@ -1,4 +1,7 @@
 def convert_decimal_to_base(n_10, base):
+    if base > 36:
+        return 'Base larger than 36, running out of alphabet'
+        
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     digits = [i if i < 10 else alphabet[i-10] for i in range(base)]
     remainders = []
@@ -13,3 +16,13 @@ test_values = [1157,5226,12166,12649]
 for x in test_values:
     rep = convert_decimal_to_base(x,23)
     print(f'({rep})_23 = ({x})_10')
+
+
+"""
+Szanowny Panie doktorze, 
+
+jestem studentem I roku Matematyki stosowanej grupy 3. Uzgodniliśmy, że nie muszę uczestniczyć w laboratoriach. Nie jestem w stanie zapisać się już dziś do projektu, wyświetla się komunikat, że nie należę do grupy lab. czwartek, co uniemożliwia mi przesłanie zadanie przez platformę Wikamp. Dlatego załączam link do githuba z rozwiązaniami: https://github.com/Legislys/Decimal-conversion
+
+ Z poważaniem,
+Aleksander Węsierski
+"""
